6ab37b5f5e9c9b24e1e2cfde2f5c8b97d2630090
Bijoy Valsan <bijoyvalsan@NB193.local>
2020-03-17 15:21:55 +0100

    mock api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 var path = require('path');
 
 const app = express();
+const randomstring = require("randomstring");
 
 app.use(helmet({
     frameguard: false,
@@ -15,6 +16,43 @@ const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     delete req.headers['X-Frame-Options'];
     res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+
+const GENDER=["MALE","FEMALE"]
+
+app.get('/api/user', (req, res) => {
+    delete req.headers['X-Frame-Options'];
+    const { query: { requestId } } = req;
+    const responseJson = {
+        id: randomstring.generate(13),
+        adId: requestId,
+        salutation: GENDER[Math.round(Math.random())],
+        firstname: randomstring.generate({
+            length: 5,
+            charset: 'alphabetic'
+          }),
+        lastname: randomstring.generate({
+            length: 5,
+            charset: 'alphabetic'
+          }),
+        birthday: "1999-12-25",
+        phoneNumber: "0049"+randomstring.generate({
+            length: 9,
+            charset: 'numeric'
+          }),
+        email: randomstring.generate({
+            length: 5,
+            charset: 'alphabetic'
+          })+randomstring.generate({
+            length: 4,
+            charset: 'numeric'
+          })+"@ssssss.sdf",
+        schufaAgreementAccepted: (Math.round(Math.random())>0),
+        newsletterSubscription: (Math.round(Math.random())>0)
+    }
+
+    res.json(responseJson)
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
